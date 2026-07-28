fe72bd6d7f9fbe0f98527bd39f6da886bbfff9a8
aPotato90 <emmanueldorgbaa19@gmail.com>
2026-07-28 23:07:09 +0100

    Add Builder-code

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,6 +1,12 @@
 import { http, createConfig, createStorage, cookieStorage } from 'wagmi'
 import { base, baseSepolia } from 'wagmi/chains'
 import { baseAccount, injected } from 'wagmi/connectors'
+import { Attribution } from "ox/erc8021";
+
+// Get your Builder Code from base.dev > Settings > Builder Codes
+const DATA_SUFFIX = Attribution.toDataSuffix({
+  codes: ["bc_fiqn9fnf"],
+});
 
 export const config = createConfig({
   chains: [base, baseSepolia],
@@ -16,6 +22,7 @@ export const config = createConfig({
     [base.id]: http('https://mainnet.base.org'),
     [baseSepolia.id]: http('https://sepolia.base.org'),
   },
+  dataSuffix: DATA_SUFFIX,
 })
 
 declare module 'wagmi' {
